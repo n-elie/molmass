7846710f8ed08d4b0b07eb50a7e7bd8f45a2f432
Christoph Gohlke <cgohlke@cgohlke.com>
2025-12-11 11:45:15 -0800

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 # molmass/tests/conftest.py
 
+"""Pytest configuration."""
+
 import os
 import sys
 
@@ -10,15 +12,15 @@ if os.environ.get('VSCODE_CWD'):
     )
 
 
-def pytest_report_header(config):
+def pytest_report_header(config: object) -> str:
+    """Return pytest report header."""
     try:
-        pyversion = f'Python {sys.version.splitlines()[0]}'
         import molmass
 
-        return '{}\npackagedir: {}\nversion: molmass {}'.format(
-            pyversion,
-            molmass.__path__[0],
-            molmass.__version__,
+        return (
+            f'Python {sys.version.splitlines()[0]}\n'
+            f'packagedir: {molmass.__path__[0]}\n'
+            f'version: molmass {molmass.__version__}'
         )
     except Exception as exc:
         return f'pytest_report_header failed: {exc!s}'
